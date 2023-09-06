f78f0fbdf467d7214a60028dd96e82e207a34e14
Darya-s <darya.stepanova95@gmail.com>
2023-09-06 08:53:23 +0200

    createing styles and adding images

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import "./Header.css";
-
+import circles from "./images/pattern-circles.svg"
 export default function Header(){
 
     return(
     <div id="header">
+        
         <div id="text-container">
+        <img alt="circles" src={circles}/>
 <p id="title" >Simple, traffic-based pricing</p>
 <p id="sub-title" >Sign-up for our 30-day trial. No credit card required.</p>
 </div>
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,11 +1,12 @@
 import Header from "./Header"
 import Container from "./Container"
 import "./MainPage.css"
-
+import background from './images/bg-pattern.svg';
 export default function MainPage(){
 
     return(
-        <div id="main">
+        <div id="main" style={{ backgroundImage: `url(${background})` }}>
+            
         <Header/>
         <Container/>
         </div>
